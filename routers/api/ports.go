a8cb138d1e781affff59d9e01d7319ffcb030bd3
jiangyuping <370615657@qq.com>
2020-03-24 17:51:38 +0800

    backup code

--- routers/api/ports.go
+++ routers/api/ports.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 	//"reflect"
 	"github.com/buger/jsonparser"
 	//"github.com/json-iterator/go"
@@ -13,26 +15,47 @@ import (
 
 func ListPorts(c *gin.Context) {
 
-	url := "http://controller:9696/v2.0/ports"
-	req, _ := http.NewRequest("GET", url, nil)
-
 	token := c.Request.Header["X-Auth-Token"][0]
+
+	url := "http://controller:9696/v2.0/ports"
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Print(err)
+		os.Exit(1)
+	}
+
+	params := req.URL.Query()
+	params.Add("fields", "name")
+	params.Add("fields", "network_id")
+	params.Add("fields", "tenant_id")
+	params.Add("fields", "device_owner")
+	params.Add("fields", "mac_address")
+	params.Add("fields", "fixed_ips")
+
+	req.URL.RawQuery = params.Encode()
 	req.Header.Add("X-Auth-Token", token)
 
-	resp, _ := http.DefaultClient.Do(req)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Print(err)
+		os.Exit(1)
+	}
+
 	defer resp.Body.Close()
 
 	body, _ := ioutil.ReadAll(resp.Body)
-	//FormatOutPutJson(body)
+	FormatOutPutJson(body)
 	//fmt.Println("---------------->", reflect.TypeOf(resp.Status), resp.Status)
 
 	//content, valueType, offset, err := jsoniter.Get(body, "ports", "[0]")
 	content, valueType, offset, err := jsonparser.Get(body, "ports", "[0]")
 
-	fmt.Println("content ===>>", string(content))
-	fmt.Println("valueType ===>>", valueType)
-	fmt.Println("offset ===>>", offset)
-	fmt.Println("err ===>>", err)
+	if err == nil {
+		fmt.Println("content ===>>", string(content))
+		fmt.Println("valueType ===>>", valueType)
+		fmt.Println("offset ===>>", offset)
+		fmt.Println("err ===>>", err)
+	}
 
 	if resp.Status == "200 OK" {
 		//FormatOutPutJson(body)
